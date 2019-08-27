9202247a4de84f555178a786d6c4a10da17e38d6
Marcel Simon <a-msimon@microsoft.com>
2019-08-27 01:36:32 +0000

    Fixed test.py in PyTorchClassification

--- PyTorchClassification/test.py
+++ PyTorchClassification/test.py
@@ -12,8 +12,10 @@ def main():
 
     image_path = '/path/to/image.jpg'
     model_path = '/path/to/model.pth.tar'
+    # [560, 560] for ensemble, [560] or [448] for single models like ResNeXt
+    model_input_sizes = [560]
 
-    model = ClassificationModel(model_path, useGPU = True)
+    model = ClassificationModel(model_path, model_input_sizes, useGPU = True)
     species, vals = model.predict_image(image_path, 3)
 
     for i in range(0, len(species)):
