a79b0c1b94f51d5033bd633bbf4b776fc9b0280e
Dan Morris <dmorris@cs.stanford.edu>
2020-04-22 14:56:34 -0700

    no message

--- classify_images.py
+++ classify_images.py
@@ -2,107 +2,99 @@
 #
 # classify_images.py
 #
-# This is a test driver for running our species classifiers and detectors.
-# The script classifies one or more hard-coded image files.
+# This is a test driver for running our species classifier.  It classifies
+# one or more hard-coded image files.  Think of this more like a demo notebook 
+# than like a script you'd actually use to run a zillion files through our
+# classifier.
 #
-# Because the inference code has not been assembled into a formal package yet,
-# you should define api_root to point to the base of our repo.  This
-# will be added to your Python path later in the script.
+# You should set the options in the 'Options' cell before running.
 #
 # This script has two non-code dependencies:
 #
-# * a classification model file (and, optionally, a detection model model)
+# * a classification model file.
+#
 # * a taxonomy file, so the scientific names used in the training data can
 #   be mapped to common names.
 #
-# Note to self... this code is compatible with pytorch 1.2, so when running on a 
-# CUDA 10.0 Linux VM:
+# By default, both point to URLs and will be downloaded to a temp directory
+# automatically.
+#
+# This code has been tested against pytorch 1.2, so dependencies can be installed as:
 #
 # conda install pytorch==1.2.0 torchvision==0.4.0 cudatoolkit=10.0 -c pytorch
+# pip install pretrainedmodels
+# pip install "pillow<7"
 #
 ####### 
 
-
-#%% Constants and imports
+#%% Imports
 
 import sys
 import os
 import pandas as pd
 import glob
+import urllib
+import tempfile
 
-# Species classification API imports deferred until later, since we have to do a little
-# path management.  This also implicitly defers PyTorch imports.
+# pip install progressbar2, not progressbar
+import progressbar
 
-# Directory to which you sync'd the repo.  Probably the same
-# directory this file lives in, but for portability, this file is set up to only
-# take dependencies on the repo according to this constant.
-api_root = r'/home/coyote/git/speciesclassification'
-subdirs_to_import = ['DetectionClassificationAPI','FasterRCNNDetection','PyTorchClassification']    
+# Species classification modules will be imported later
 
-# Path to taxa.csv, for latin --> common mapping
-#
-# Set to None to disable latin --> common mapping
-taxonomy_path = r'/data/species_classification/taxa.19.08.28.0536.csv' # None
 
-job_name = ''
+#%% Options
+
+# Species classification API imports deferred until later, since we have to do a 
+# little path management.  This also implicitly defers PyTorch imports.
+
+# Directory to which you sync'd this repo.
+api_root = r'c:\git\speciesclassification'
+
+# If not None, pre-pended to filenames.  Most useful when filenames are coming from 
+# a .csv file.
 images_to_classify_base = None
 
 # images_to_classify can be:
 #
 # an array of filenames
 #
-# a single string; if it's a string, it's assumed to point to a .csv file, in 
-# which each row is [filename,description]
+# a .csv file
+#
+# a single image file
 #
 # a directory, which is recursively enumerated
-if False:
-    images_to_classify = [
-            '/data/species_classification/coyote.jpg',
-            '/data/species_classification/meerkat.jpg',
-            '/data/species_classification/elephant.jpg'
-            ]
+images_to_classify = r'g:\temp\elephant.jpg'
+# images_to_classify = [r'g:\temp\elephant.jpg']
+# images_to_classify = 'image_list.csv'
+# images_to_classify = r'g:\temp\animal_images'
 
-# Pick images from a .csv file
-if False:
-    images_to_classify = '/data/species_classification/animal_list.2018.10.23.12.58.16.csv'
-    images_to_classify_base = '/data/species_classification/sample_animals'
-    
-# Pick images from a folder
-if False:
-    images_to_classify = '/data/species_classification/elephants_and_hippos'
-    
-# Pick images from a folder
-if True:
-    images_to_classify = '/data/species_classification/images/sample_images.2019.12.28'
-    job_name = 'sample_images.2019.12.28'
-    
 # Classification results will be written here
-classification_output_file = None
+classification_output_file = 'g:\temp\classification_output.csv'
 
-model_base = '/data/species_classification/models'
+# Path to taxa.csv, for latin --> common mapping
+#
+# Set to None to disable latin --> common mapping
+#
+# If a URL, will be automatically downloaded to a temp folder.
+taxonomy_path = 'https://lilablobssc.blob.core.windows.net/models/species_classification/species_classification.2019.12.00.taxa.csv'
 
-# 2019 fixed model
-# classification_model_path = os.path.join(model_base,'iNat_all_extended/demosite-model-ensemble-resnext-inceptionV4-560-83.1/iNat_all_extended_ensemble_resnext_inceptionV4_560_83.1_model.2019.12.00.pytorch')
+# Path to our classification model file.
+#
+# If a URL, will be automatically downloaded to a temp folder.
+classification_model_path = 'https://lilablobssc.blob.core.windows.net/models/species_classification/species_classification.2019.12.00.pytorch'
 
-# 2019 broken model
-# classification_model_path = os.path.join(model_base,'iNat_all_extended_buggy/demosite-model-ensemble-resnext-inceptionV4-560-81.0/iNat_all_extended_ensemble_resnext_inceptionV4_560_81.9_model.2019.10.00.pytorch')
-
-# 2018 model    
-classification_model_path = os.path.join(model_base,'iNat_original/inc4-incres2-560-78.5/inc4-incres2-560-78.5.model_deploy.pth.tar')
-
-assert(os.path.isfile(classification_model_path))
-
-output_base = '/data/species_classification/output'
-model_name = os.path.basename(classification_model_path)
-classification_output_file = os.path.join(output_base,'classifications_{}_{}.csv'.format(job_name,model_name))
-    
 # Detection (i.e., bounding box generation) is optional; set to None 
 # to disable detection
 detection_model_path = None
 
 # This must be True if detection is enabled.  Classification can be run
 # on the CPU or GPU.
-use_gpu = True
+use_gpu = False
+
+
+#%% Constants 
+
+subdirs_to_import = ['DetectionClassificationAPI','FasterRCNNDetection','PyTorchClassification']    
 
 # List of image sizes to use, one per model in the ensemble.  Images will be resized 
 # and reshaped to square images prior to classification.  
@@ -111,7 +103,6 @@ use_gpu = True
 # ensemble. For ResNext, we typically specify [448].
 #
 image_sizes = [560, 560]
-# image_sizes = [448]
 
 mak_k_to_print = 3
 debug_max_images = -1
@@ -121,13 +112,13 @@ debug_max_images = -1
 
 if (not api_root.lower() in map(str.lower,sys.path)):
     
-    print("Adding {} to the python path".format(api_root))
+    print('Adding {} to the python path'.format(api_root))
     sys.path.insert(0,api_root)
 
 for s in subdirs_to_import:
     if (not s.lower() in map(str.lower,sys.path)):
         import_path = os.path.join(api_root,s)
-        print("Adding {} to the python path".format(import_path))
+        print('Adding {} to the python path'.format(import_path))
         sys.path.insert(0,import_path)    
 
 
@@ -140,9 +131,9 @@ import api as speciesapi
 
 latin_to_common = {}
 
-if taxonomy_path != None:
+if taxonomy_path is not None:
         
-    print("Reading taxonomy file")
+    print('Reading taxonomy file')
     
     # Read taxonomy file; takes ~1 minute
     df = pd.read_csv(taxonomy_path)
@@ -161,7 +152,7 @@ if taxonomy_path != None:
         latin_name = row['scientificName']
         latin_name = latin_name.strip()
         if len(latin_name)==0:
-            print("Warning: invalid scientific name at {}".format(index))
+            print('Warning: invalid scientific name at {}'.format(index))
             latin_name = 'unknown'
         common_name = row['vernacularName']
         common_name = common_name.strip()
@@ -169,66 +160,142 @@ if taxonomy_path != None:
         common_name = common_name.lower()
         latin_to_common[latin_name] = common_name
     
-    print("Finished reading taxonomy file")
+    print('Finished reading taxonomy file')
 
 
-#%% Latin-->common lookup
+#%% Support functions
+
+class DownloadProgressBar():
+    '''
+    Console progress indicator for downloads.
+    
+    stackoverflow.com/questions/37748105/how-to-use-progressbar-module-with-urlretrieve
+    '''
+    
+    def __init__(self):
+        self.pbar = None
+
+    def __call__(self, block_num, block_size, total_size):
+        if not self.pbar:
+            self.pbar = progressbar.ProgressBar(max_value=total_size)
+            self.pbar.start()
+            
+        downloaded = block_num * block_size
+        if downloaded < total_size:
+            self.pbar.update(downloaded)
+        else:
+            self.pbar.finish()
+            
+            
+def download_url(url, destination_filename=None, progress_updater=None, force_download=False, 
+                 temp_dir=None):
+    '''
+    Download a URL to a temporary file
+    '''
+
+    if progress_updater is None:
+        progress_updater = DownloadProgressBar()
+        
+    if temp_dir is None:
+        temp_dir = os.path.join(tempfile.gettempdir(),'species_classification')
+        os.makedirs(temp_dir,exist_ok=True)
+        
+    # This is not indended to gurantee uniqueness, we just know it happens to guarantee
+    # uniqueness for this application.
+    if destination_filename is None:
+        url_as_filename = url.replace('://', '_').replace('.', '_').replace('/', '_')
+        destination_filename = \
+            os.path.join(temp_dir,url_as_filename)
+    if (not force_download) and (os.path.isfile(destination_filename)):
+        print('Bypassing download of already-downloaded file {}'.format(os.path.basename(url)))
+        return destination_filename
+    print('Downloading file {}'.format(os.path.basename(url)),end='')
+    urllib.request.urlretrieve(url, destination_filename, progress_updater)  
+    assert(os.path.isfile(destination_filename))
+    nBytes = os.path.getsize(destination_filename)
+    print('...done, {} bytes.'.format(nBytes))
+    return destination_filename
+
 
 def do_latin_to_common(latin_name):
-
+    '''
+    Latin --> common lookup
+    '''
+    
     if len(latin_to_common) == 0:
         return latin_name
     
     latin_name = latin_name.lower()
     if not latin_name in latin_to_common:
-        print("Warning: latin name {} not in lookup table".format(latin_name))
+        print('Warning: latin name {} not in lookup table'.format(latin_name))
         common_name = latin_name
     else:
         common_name = latin_to_common[latin_name]
         common_name = common_name.strip()
         
     if (len(common_name) == 0):
-        print("Warning: empty result for latin name {}".format(latin_name))
+        print('Warning: empty result for latin name {}'.format(latin_name))
         common_name = latin_name
 
     return common_name
 
 
-#%% Create the model(s)
+#%% Download models if necessary
 
-assert os.path.isfile(classification_model_path)
-if detection_model_path != None:
+if classification_model_path.startswith('http'):
+    classification_model_path = download_url(classification_model_path)
+assert(os.path.isfile(classification_model_path))    
+
+if taxonomy_path.startswith('http'):
+    taxonomy_path = download_url(taxonomy_path)
+assert(os.path.isfile(taxonomy_path))    
+
+if detection_model_path is not None:
+    detection_model_path = download_url(detection_model_path)
     assert os.path.isfile(detection_model_path)
 
-print("Loading model")
+
+#%% Create the model(s)
+
+
+print('Loading model')
 model = speciesapi.DetectionClassificationAPI(classification_model_path, 
-                                              detection_model_path, image_sizes, use_gpu)
-print("Finished loading model")
+                                              detection_model_path,
+                                              image_sizes, 
+                                              use_gpu)
+print('Finished loading model')
 
 
 #%% Prepare the list of images and query names
 
 queries = None
 
+# If we specified a single image
+if isinstance(images_to_classify,str) and (not images_to_classify.endswith('.csv')):
+    images_to_classify = [images_to_classify]
+    
+# If we specified a folder    
 if isinstance(images_to_classify,str) and os.path.isdir(images_to_classify):
     
     images = glob.glob(os.path.join(images_to_classify,'**/*.*'), recursive=True)
     images = [fn for fn in images if os.path.isfile(fn)]
     queries = [os.path.basename(os.path.dirname(fn)) for fn in images]
     print('Loaded a folder of {} images'.format(len(images)))    
+
+# If we specified a .csv file    
+elif isinstance(images_to_classify,str) and images_to_classify.endswith('.csv'):
     
-elif isinstance(images_to_classify,str) and os.path.isfile(images_to_classify):
-    
-    print("Reading image list file")
+    print('Reading image list file')
     df_images = pd.read_csv(images_to_classify,header=None)
     df_images.columns = ['filename','query_string']
-    nImages = len(images)    
-    print("Read {} image names".format(len(images)))
+    n_images = len(images)    
+    print('Read {} image names'.format(len(images)))
     images = list(df_images.filename)
     queries = list(df_images.query_string)
     assert(len(queries) == len(images))
-    
-else:
+
+# If we specified a list    
+elif isinstance(images_to_classify,list):
     
     assert isinstance(images_to_classify,list)
     images = images_to_classify
@@ -238,9 +305,9 @@ else:
 
 #%% Classify images
 
-nErrors = 0
-nImagesClassified = 0
-nImages = len(images)
+n_errors = 0
+n_images_classified = 0
+n_images = len(images)
 
 if classification_output_file is not None:
     f = open(classification_output_file,'w+')
@@ -248,13 +315,13 @@ if classification_output_file is not None:
 # i_fn = 1; fn = images[i_fn]    
 for i_fn,fn in enumerate(images):
     
-    print("Processing image {} of {}".format(i_fn,nImages))
+    print('Processing image {} of {}'.format(i_fn,n_images))
     fn = fn.replace('\\','/')
     query = ''
     if queries is not None:
         query = queries[i_fn]
         
-    if images_to_classify_base is not None and len(images_to_classify_base > 0):
+    if images_to_classify_base is not None:
         fn = os.path.join(images_to_classify_base,fn)
 
     # with torch.no_grad():
@@ -263,11 +330,11 @@ for i_fn,fn in enumerate(images):
     try:
         prediction = model.predict_image(fn, topK=min(5,mak_k_to_print), multiCrop=False, 
                                              predict_mode=speciesapi.PredictMode.classifyOnly)
-        nImagesClassified = nImagesClassified + 1
+        n_images_classified = n_images_classified + 1
         
     except Exception as e:
-        print("Error classifying image {} ({}): {}".format(i_fn,fn,str(e)))
-        nErrors = nErrors + 1
+        print('Error classifying image {} ({}): {}'.format(i_fn,fn,str(e)))
+        n_errors = n_errors + 1
         continue
 
     # i_prediction = 0
@@ -276,7 +343,7 @@ for i_fn,fn in enumerate(images):
         likelihood = prediction.species_scores[i_prediction]
         likelihood = '{0:0.3f}'.format(likelihood)
         common_name = do_latin_to_common(latin_name)
-        s = '"{}","{}","{}","{}","{}","{}","{}"'.format(
+        s = '"{}","{}","{}","{}",{}","{}","{}"'.format(
                 i_fn,fn,query,i_prediction,latin_name,common_name,likelihood)
         if classification_output_file is not None:
             f.write(s + '\n')
@@ -290,4 +357,4 @@ for i_fn,fn in enumerate(images):
 if classification_output_file is not None:
     f.close()
     
-print("Finished classifying {} of {} images ({} errors)".format(nImagesClassified,nImages,nErrors))
+print('Finished classifying {} of {} images ({} errors)'.format(n_images_classified,n_images,n_errors))
